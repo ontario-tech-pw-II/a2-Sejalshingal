3bfc7134a9613d3b6c1ab267119338e57dd74da8
Sejalshingal <56410993+Sejalshingal@users.noreply.github.com>
2020-01-23 17:53:34 -0500

    Update mycp.cpp

--- mycp.cpp
+++ mycp.cpp
@@ -2,21 +2,31 @@ int main(int argc, char const *argv[])
 {
  	ifstream fin;
  	ofstream fout;
+	string afile = argv[1];
+	string bfile = argv[2];
+	int x=0;
 	
 	// check if there are enough arguments
+	for(int i=0; i<argc; i++)
+		x++;
 	
-	
+	if(x<2){
+		cout<<"NOT ENOUGH ARGUMENTS"<<endl;
+		exit(0);
+	}
+		
+	fin.open(afile);
 	// open the first file
  	
 	char c;
 
- 	if (fin.fail()) // check if it is successful 
+	if (fin.fail()) // check if it is successful 
  	{
  		cerr << " Cannot open the input file!" << endl;
  		return 1;
  	}
  	
-
+	fout.open(bfile);
 	// open the second file
 	
  	if (fout.fail())
